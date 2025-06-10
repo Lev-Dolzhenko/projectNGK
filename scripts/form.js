f173f41586551ccaadbb4129cb93d8bededf98bf
Leo <workspace2707@yandex.ru>
2025-06-10 23:52:54 +0300

    created project

--- scripts/form.js
+++ scripts/form.js
@@ -1,31 +1,26 @@
+
 document.getElementById("adviceForm").addEventListener("submit", async (e) => {
   e.preventDefault();
   const form = e.target;
   const formData = new FormData(form);
-
-  // Преобразуем FormData в объект
   const data = {};
   formData.forEach((value, key) => (data[key] = value));
 
-  // URL вашего Google Apps Script
-  const GAS_URL =
-    "https://script.google.com/macros/s/AKfycbwR1fhQCUbHHlROtfHS1bfXPj4-pPkkeIdAQB_9SxhCIaJuQ-joXKn0Mfpk-aSnor-Stg/exec";
+  const GAS_URL = "https://script.google.com/macros/s/AKfycbySXcZVP3lYOnqhmUDsRCb6tUCmYZKBwgjzra4m2ibG-mTMbn901zmCG3B649o2dDY/exec";
 
   try {
     const response = await fetch(GAS_URL, {
       method: "POST",
-      mode: "cors", // Явно указываем режим CORS
       headers: {
-        "Content-Type": "application/json",
-        Origin: window.location.origin, // Добавляем Origin
+        "Content-Type": "application/json"
       },
-      body: JSON.stringify(data),
+      body: JSON.stringify(data)
     });
 
-    const result = await response.json(); // Измените на .json() вместо .text()
+    const result = await response.json();
     console.log("Успешно:", result);
-    form.reset();
     alert(result.message || "Заявка отправлена!");
+    form.reset();
   } catch (error) {
     console.error("Ошибка:", error);
     alert("Ошибка отправки. Попробуйте позже.");
